cda727600ba7ab0c87b8ec714e433e34120d0cb5
demasiadopedro <thepedrosantos@proton.me>
2026-05-11 22:54:54 -0300

    feat: Implementação dos roles guards nas rotas patient

--- backend/src/patient/patient.controller.ts
+++ backend/src/patient/patient.controller.ts
@@ -6,12 +6,19 @@ import {
 	Delete,
 	Param,
 	Body,
+	UseGuards,
 } from '@nestjs/common';
 import { PatientService } from './patient.service';
 import { CreatePatientDto } from './dto/createPatient.dto';
 import { UpdatePatientDto } from './dto/updatePatient.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RoleGuard } from '../auth/role/role.guard';
+import { Roles } from '../auth/roles/roles.decorator';
+import { Role } from '@prisma/client';
 
 @Controller('pacientes')
+@UseGuards(JwtAuthGuard, RoleGuard)
+@Roles(Role.ADMIN, Role.MANAGER)
 export class PatientController {
 	constructor(private readonly patientService: PatientService) {}
 
