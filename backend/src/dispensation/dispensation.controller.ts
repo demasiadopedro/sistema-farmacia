7eadcddc1c77a4dbbdfe2a4b597bbfc879cbb9e2
demasiadopedro <thepedrosantos@proton.me>
2026-05-11 22:52:13 -0300

    feat: Implementação dos roles guards nas rotas de dispensation

--- backend/src/dispensation/dispensation.controller.ts
+++ backend/src/dispensation/dispensation.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Post, Body, Param, ParseUUIDPipe } from '@nestjs/common';
+import {
+	Controller,
+	Post,
+	Body,
+	Param,
+	ParseUUIDPipe,
+	UseGuards,
+} from '@nestjs/common';
 import { DispensationService } from './dispensation.service';
 import { CreateDispensationDto } from './dto/create-dispensation.dto';
 import { ReturnDispensationDto } from './dto/return-dispensation.dto';
+import { Roles } from '../auth/roles/roles.decorator';
+import { Role } from '@prisma/client';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RoleGuard } from '../auth/role/role.guard';
 
 @Controller('dispensation')
+@UseGuards(JwtAuthGuard, RoleGuard)
+@Roles(Role.ADMIN, Role.MANAGER)
 export class DispensationController {
 	constructor(private readonly dispensationService: DispensationService) {}
 
