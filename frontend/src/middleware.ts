3ec8f866dd7988fe216864581195645712cd61e5
demasiadopedro <thepedrosantos@proton.me>
2026-05-24 16:19:46 -0300

    feat: middleware pra interceptar as request

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const token = request.cookies.get('acessToken')?.value
+    const pathName = request.nextUrl.pathname
+
+    const publicRoutes = ['/login']
+    const isPublicRoute = publicRoutes.includes(pathName)
+
+    if(!token && !isPublicRoute) {
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+
+    if (token && pathName === '/login'){
+        return NextResponse.redirect(new URL('/dashboard', request.url))
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
