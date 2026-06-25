9179f69f3579806764009f9122e74a2c7be33a18
demasiadopedro <thepedrosantos@proton.me>
2026-06-25 09:28:00 -0300

    fix: Corrigindo URLs dos endpoints no frontend

--- frontend/src/app/estoque/page.tsx
+++ frontend/src/app/estoque/page.tsx
@@ -36,7 +36,7 @@ export default async function EstoquePage() {
         console.log("[*] ERRO DO NESTJS!!!:", response.status, await response.text());
     }
 
-    const medicamentosResponse = await fetch(`${process.env.URL_BACKEND}remedy`, {
+    const medicamentosResponse = await fetch(`${process.env.URL_BACKEND}/remedy`, {
         headers: { Authorization: `Bearer ${token}` }
     });
     const medicamentos = await medicamentosResponse.json();
